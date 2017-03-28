2c95ea4d015f91a67a3cd50381a7e290ba35ca6b
LiangZhipeng <mingjiameng@sina.com>
2017-03-28 17:33:52 +0800

    Initial Commit

---
+++ PaperRMS/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PaperRMS
+//
+//  Created by 梁志鹏 on 2017/3/28.
+//  Copyright © 2017年 overcode. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
